d3c8d0ec250bf71afd5a83c676b766418adebd1b
Nam Hoang <nh99@uw.edu>
2021-05-16 14:17:09 -0700

    update push implementation

--- TCSS450Team2Client/app/src/main/java/edu/uw/tcss450team2client/ui/chat/ChatRoom.java
+++ TCSS450Team2Client/app/src/main/java/edu/uw/tcss450team2client/ui/chat/ChatRoom.java
@@ -1,10 +1,19 @@
 package edu.uw.tcss450team2client.ui.chat;
 
+import android.content.Context;
+
 import java.io.Serializable;
 
 public class ChatRoom implements Serializable {
 
+
     private final int mChatId;
+
+    private  String mChatRoomName;
+
+    private final Context mContext;
+
+    private
 
     public ChatRoom(final int chatId) {
         mChatId = chatId;
