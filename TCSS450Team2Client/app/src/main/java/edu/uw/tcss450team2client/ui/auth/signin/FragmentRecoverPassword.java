1d9ca0f0208c1b1cb78e9c82f343d884224af197
nstickler28 <48421055+nstickler28@users.noreply.github.com>
2021-05-18 12:40:24 -0700

    recover password implementation.  App crashes on navigation

---
+++ TCSS450Team2Client/app/src/main/java/edu/uw/tcss450team2client/ui/auth/signin/FragmentRecoverPassword.java
@@ -0,0 +1,159 @@
+package edu.uw.tcss450team2client.ui.auth.signin;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import org.json.JSONObject;
+
+import edu.uw.tcss450team2client.R;
+import edu.uw.tcss450team2client.databinding.FragmentRecoverPasswordBinding;
+import edu.uw.tcss450team2client.utils.PasswordValidator;
+
+import static edu.uw.tcss450team2client.utils.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.tcss450team2client.utils.PasswordValidator.checkPwdLength;
+import static edu.uw.tcss450team2client.utils.PasswordValidator.checkPwdSpecialChar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentRecoverPassword extends Fragment {
+
+    private FragmentRecoverPasswordBinding binding;
+
+    private RecoverPasswordViewModel mRecoverPasswordModel;
+
+    /**
+     * Method to validate email.
+     */
+    private PasswordValidator mEmailValidator = checkPwdLength(2)
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
+
+    /**
+     * Empty public constructor.
+     */
+    public FragmentRecoverPassword() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRecoverPasswordModel = new ViewModelProvider(getActivity())
+                .get(RecoverPasswordViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentRecoverPasswordBinding.inflate(inflater);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSendRecover.setOnClickListener(this::attemptRecovery);
+
+        mRecoverPasswordModel.addResponseObserver(getViewLifecycleOwner(),
+                this::observeResponse);
+
+        FragmentRecoverPasswordArgs args = FragmentRecoverPasswordArgs.fromBundle(getArguments());
+
+        binding.editEmail.setText(args.getEmail().equals("default") ? "" : args.getEmail());
+    }
+
+    /**
+     * Creates a dialog box that acknowledges the users input to recover password.
+     */
+    private void createDialogAcknowledge() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.textview_changePassword_headMessage);
+        builder.setMessage(R.string.textview_changePassword_description);
+        builder.setTitle(R.string.text_forgot_password);
+        builder.setPositiveButton(R.string.button_recoverPassword_send, (dialog, which) -> {
+            Log.d("Recovery", "Acknowledge");
+        });
+        builder.create();
+        builder.show();
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to  PasswordRecoveryViewModel
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            //don't want the client "user" to know if the email was correct or not?
+            //email was or wasn't send, we want to create a dialog possible success dialog
+            createDialogAcknowledge();
+
+        } else {
+            Log.d("JSON Response", "No Response");
+
+        }
+    }
+
+    /**
+     * Navigates the user to login fragment.
+     */
+    private void navigateToLogin() {
+        FragmentRecoverPasswordDirections.ActionFragmentRecoverPasswordToSignInFragment directions =
+                FragmentRecoverPasswordDirections.actionFragmentRecoverPasswordToSignInFragment();
+
+        directions.setEmail(binding.editEmail.getText().toString());
+        Navigation.findNavController(getView()).navigate(directions);
+    }
+
+    /**
+     * Helper method to being sequential validation process.
+     *
+     * @param button login button.
+     */
+    private void attemptRecovery(final View button) {
+        Log.d("Recovery", "User selected to recover password");
+        validateEmail();
+    }
+
+    /**
+     * Helper method to validate email.  Sets error message
+     * if incorrect input is given.  Calls validate password method.
+     */
+    private void validateEmail() {
+        mEmailValidator.processResult(
+                mEmailValidator.apply(binding.editEmail.getText().toString().trim()),
+                this::sendRecoveryRequest,
+                result -> binding.editEmail.setError("Please enter a valid Email address."));
+    }
+    /**
+     * Method to send recovery request.
+     */
+    private void sendRecoveryRequest() {
+        //recover the users password
+        mRecoverPasswordModel.connect(binding.editEmail.getText().toString());
+    }
+}
---
+++ TCSS450Team2Client/app/src/main/java/edu/uw/tcss450team2client/ui/auth/signin/RecoverPasswordViewModel.java
@@ -0,0 +1,100 @@
+package edu.uw.tcss450team2client.ui.auth.signin;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+public class RecoverPasswordViewModel extends AndroidViewModel {
+
+    private MutableLiveData<JSONObject> mResponse;
+
+    public RecoverPasswordViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    /**
+     * Adds a response observer.
+     *
+     * @param owner the owner
+     * @param observer the observer
+     */
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        mResponse.observe(owner, observer);
+    }
+
+    /**
+     * Handles errors for connecting to a WebService endpoint.
+     * @param error
+     */
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset())
+                    .replace('\"', '\'');
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "code:" + error.networkResponse.statusCode +
+                        ", data:\"" + data +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+    }
+
+    /**
+     * Connects to end point to send forgot password link to email.
+     *
+     * @param email registered email
+     */
+    public void connect(final String email) {
+        String url = "https://tcss450-team2-server.herokuapp.com";  // Does this use auth endpoint or new endpoint??
+        JSONObject body = new JSONObject();
+        try {
+            body.put("email", email);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Request request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                body,
+                mResponse::setValue,
+                this::handleError);
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
+}
