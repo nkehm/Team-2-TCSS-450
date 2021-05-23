61e32ff9a05a0da5cc354ff4466dcb127648d93b
nkehm <62219855+nkehm@users.noreply.github.com>
2021-05-23 15:45:54 -0700

    Revert "Bug in the xml causing app to crash on verification navigation - fixed."
    This reverts commit f27e80262830b69a83e95db407f2805d554634f6.

--- TCSS450Team2Client/app/src/main/java/edu/uw/tcss450team2client/ui/auth/verification/VerificationFragment.java
+++ TCSS450Team2Client/app/src/main/java/edu/uw/tcss450team2client/ui/auth/verification/VerificationFragment.java
@@ -1,6 +1,5 @@
 package edu.uw.tcss450team2client.ui.auth.verification;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450team2client.R;
-import edu.uw.tcss450team2client.databinding.FragmentVerificationBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -34,16 +32,15 @@ public class VerificationFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_verification, container, false);
     }
 
-    @SuppressLint("SetTextI18n")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        VerificationFragmentArgs args = VerificationFragmentArgs.fromBundle(getArguments());
+        V args = VerifyFragmentArgs.fromBundle(getArguments());
 
-        FragmentVerificationBinding binding = FragmentVerificationBinding.bind(getView());
-        binding.editPersonalGreeting.setText("Hello " + args.getFirstname() + " " +
-                args.getLastname() + "! Please confirm your email at " + args.getEmail() +
+        FragmentVerifyBinding binding = FragmentVerifyBinding.bind(getView());
+        binding.editPersonalGreeting.setText("Hello " + args.getFirstName() + " " +
+                args.getLastName() + "! Please confirm your email at " + args.getEmail() +
                 " so you can start enjoying appName");
 
         binding.buttonVerify.setOnClickListener(button -> navigateToLogin(args));
@@ -54,9 +51,9 @@ public class VerificationFragment extends Fragment {
      *
      * @param args args used update login upon verification.
      */
-    private void navigateToLogin(VerificationFragmentArgs args) {
-        VerificationFragmentDirections.ActionVerificationFragmentToSignInFragment directions =
-                VerificationFragmentDirections.actionVerificationFragmentToSignInFragment();
+    private void navigateToLogin(VerifyFragmentArgs args) {
+        VerifyFragmentDirections.ActionVerifyFragmentToSignInFragment directions =
+                VerifyFragmentDirections.actionVerifyFragmentToSignInFragment();
         directions.setEmail(args.getEmail());
         directions.setPassword(args.getPassword());
         Navigation.findNavController((getView())).navigate(directions);
